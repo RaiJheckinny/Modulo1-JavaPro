ad18174d10ab9d8ea77e7ecf98b72aee2b8bc788
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-04 16:36:50 -0300

    feart: exercio de no 2

--- modulo02/src/Main.java
+++ modulo02/src/Main.java
@@ -1,5 +1,18 @@
+import java.math.BigInteger;
+
 public class Main {
+    public static BigInteger numberMultiplicado = new BigInteger("1");
+    public static final BigInteger calcularFimonat(BigInteger number){
+        if (number.equals(new BigInteger("0"))){
+            return numberMultiplicado;
+        }
+        numberMultiplicado = numberMultiplicado.multiply(number);
+
+        return calcularFimonat(number.subtract(new BigInteger("1")));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BigInteger n = new BigInteger("100");
+        System.out.println("Fibonate de " + n + " e: " + calcularFimonat(n));
     }
 }
