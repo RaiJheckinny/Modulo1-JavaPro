55d45054dac4f5c7e2d960ac7dc82a8efa0ff07c
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-05 03:46:02 -0300

    feart: exercio de no 1

--- modulo02/src/Main.java
+++ modulo02/src/Main.java
@@ -11,7 +11,19 @@ public class Main {
         return calcularFimonat(number.subtract(new BigInteger("1")));
     }
 
+    public static final Long fatorial(Long n){
+        if(n.equals(0L) || n.equals(1L)){
+            return 1L;
+        }
+        return n * fatorial(n-1);
+    }
+
     public static void main(String[] args) {
+        //Exercicio 01
+        Long y = 100L;
+        System.out.println("Fibonate de " + y + " e: " + fatorial(y));
+
+        //Exercicio 02
         BigInteger n = new BigInteger("100");
         System.out.println("Fibonate de " + n + " e: " + calcularFimonat(n));
     }
