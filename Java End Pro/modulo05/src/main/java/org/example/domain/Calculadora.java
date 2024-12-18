b0149d26a4619a583e8a6bb02345a9d45903347d
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-12-17 21:50:46 -0300

    feat: modulo 5 exercicio 1

---
+++ Java End Pro/modulo05/src/main/java/org/example/domain/Calculadora.java
@@ -0,0 +1,16 @@
+package org.example.domain;
+
+public class Calculadora {
+    public int adicionar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+    public int subtrair(int numero1, int numero2){
+        return numero1 - numero2;
+    }
+    public int multiplicar(int numero1, int numero2){
+        return numero1 * numero2;
+    }
+    public int dividir(int numero1, int numero2){
+        return numero1 / numero2;
+    }
+}
